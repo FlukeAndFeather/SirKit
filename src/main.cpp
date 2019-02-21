28a111773ae25b0289cdba7db375a8fd4cbff7e0
Max Czapanskiy <maxczap@stanford.edu>
2019-02-21 09:50:30 -0800

    IR sensor emits and receives

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
 
 /*---------------Constants----------------------------------*/
-#define INPUT_PIN 19
+#define IR_E_PIN 21
+#define IR_R_PIN 19
 
 /*---------------Module Function Prototypes-----------------*/
 
@@ -11,11 +12,23 @@
 
 /*---------------Teensy Main Functions----------------------*/
 void setup() {
-  // put your setup code here, to run once:
+  // Connect to Serial
+  Serial.begin(9600);
+  while(!Serial);
+
+  // Set pin input/outputs
+  pinMode(IR_E_PIN, OUTPUT);
+  pinMode(IR_R_PIN, INPUT);
+
+  // Emit IR
+  digitalWrite(IR_E_PIN, HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Check for signal
+  uint16_t ir_signal = analogRead(IR_R_PIN);
+  Serial.println(ir_signal);
+  delay(1000);
 }
 
 /*----------------Module Functions--------------------------*/
