c0557b4bd7183621885add2bc802576d14fcccc0
Max Czapanskiy <maxczap@stanford.edu>
2019-02-21 10:20:41 -0800

    IR emits at 10kHz

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,15 @@
 /*---------------Module Function Prototypes-----------------*/
 
 
-/*---------------Module Variables---------------------------*/
+/*---------------Interrupt Handlers Prototypes--------------*/
+void toggle_ir(void);
 
+/*---------------Module Variables---------------------------*/
+uint16_t ir_freq = 10000; // 10kHz
+uint16_t ir_period = 1000000 / ir_freq; // in microseconds
+uint16_t ir_switch = ir_period / 2;
+IntervalTimer ir_timer;
+volatile bool ir_high;
 
 /*---------------Teensy Main Functions----------------------*/
 void setup() {
@@ -19,16 +26,24 @@ void setup() {
   // Set pin input/outputs
   pinMode(IR_E_PIN, OUTPUT);
   pinMode(IR_R_PIN, INPUT);
-
-  // Emit IR
-  digitalWrite(IR_E_PIN, HIGH);
+  
+  ir_timer.begin(toggle_ir, ir_switch);
+  ir_high = true;
 }
 
 void loop() {
   // Check for signal
+  /*
   uint16_t ir_signal = analogRead(IR_R_PIN);
   Serial.println(ir_signal);
   delay(1000);
+  */
 }
 
 /*----------------Module Functions--------------------------*/
+
+/*---------------Interrupt Handlers-------------------------*/
+void toggle_ir(void) {
+  digitalWrite(IR_E_PIN, ir_high ? HIGH : LOW);
+  ir_high = !ir_high;
+}
