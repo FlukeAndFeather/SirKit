79d483da0ffaafb324610a95f4165583895dbd7a
Max Czapanskiy <maxczap@stanford.edu>
2019-02-21 09:28:46 -0800

    Repo initialization

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+/*---------------Constants----------------------------------*/
+#define INPUT_PIN 19
+
+/*---------------Module Function Prototypes-----------------*/
+
+
+/*---------------Module Variables---------------------------*/
+
+
+/*---------------Teensy Main Functions----------------------*/
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+/*----------------Module Functions--------------------------*/
